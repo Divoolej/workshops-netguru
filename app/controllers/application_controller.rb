dd6fbe35e15aa9d4d0fac727dfee3a65eda15cd2
Divoolej <divoolej@gmail.com>
2015-07-18 23:58:14 +0200

    The actual solution to issue #2 Oups. Add strong parameters to Devise Users Controller (for firstname and lastname)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
-
+  before_action :configure_devise_parameters, if: :devise_controller?
+  
+  
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
   protect_from_forgery with: :exception
+  
+  protected
+    
+    def configure_devise_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |user| user.permit(:firstname, :lastname, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:account_update) { |user| user.permit(:firstname, :lastname, :email, :password, :password_confirmation, :current_password) }
+      devise_parameter_sanitizer.for(:sign_in) { |user| user.permit(:email, :password) }
+    end
 end
